25db55725d52a5902af4211b1b390ce2bb76d889
Nurdin Osmonov <oooooxg@gmail.com>
2022-10-25 18:07:06 +0600

    Level-3 Completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,35 @@ import Card from "./lib/Card";
 import CardDeck from "./lib/CardDeck";
 
 function App() {
+  const [cards, setCards] = useState<Card[]>([]);
 
   const deck = new CardDeck();
   const round = deck.getCards(5);
 
+  const playPoker = () => {
+    setCards(round);
+  }
 
+  const button = <button className="playButton" onClick={playPoker}>Play</button>;
 
+  if (cards.length === 0) {
+    return (
+      <div>
+        {button}
+      </div>
+    )
+  } else {
     return (
       <div className="App">
-
+        {button}
+        <CardView rank={round[0].rank} suit={round[0].suit}/>
+        <CardView rank={round[1].rank} suit={round[1].suit}/>
+        <CardView rank={round[2].rank} suit={round[2].suit}/>
+        <CardView rank={round[3].rank} suit={round[3].suit}/>
+        <CardView rank={round[4].rank} suit={round[4].suit}/>
       </div>
     );
-
+  }
 }
 
 export default App;
